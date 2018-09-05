4a29c2399d637107973fc5d1524052b6d62ece92
Bs, Bharatraj (US - Hermitage) <bhbs@deloitte.com>
2018-09-05 07:34:01 -0500

    Add project files.

---
+++ nonAttest/Features/login.feature
@@ -0,0 +1,7 @@
+﻿Feature: Login
+
+Scenario: Login to MAT
+Given I am in Authentication Page
+When I enter valid credentials
+And I click on Submit
+Then I should be navigated to MAT Home Page
---
+++ nonAttest/Steps/login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace nonAttest.Steps
+{
+    class login
+    {
+
+        [Given(@"I am in Authentication Page")]
+        public void GivenIAmInAuthenticationPage()
+        {
+           
+        }
+
+        [When(@"I enter valid credentials")]
+        public void WhenIEnterValidCredentials()
+        {
+           
+        }
+
+        [When(@"I click on Submit")]
+        public void WhenIClickOnSubmit()
+        {
+           
+        }
+
+        [Then(@"I should be navigated to MAT Home Page")]
+        public void ThenIShouldBeNavigatedToMATHomePage()
+        {
+            
+        }
+
+    }
+}
